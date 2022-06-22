667e8d17d5d79f5f26f3e5628004e57f112c88f8
Phat Duong <beebapcay@gmail.com>
2022-06-22 15:32:15 +0700

    Init `p2p-server` by `express-generator-typescript`

---
+++ app/p2p-server/src/index.ts
@@ -0,0 +1,13 @@
+import logger from 'jet-logger';
+import './pre-start'; // Must be the first import
+import server from './server';
+
+
+// Constants
+const serverStartMsg = 'Express server started on port: ',
+  port = (process.env.PORT || 3000);
+
+// Start server
+server.listen(port, () => {
+    logger.info(serverStartMsg + port);
+});
