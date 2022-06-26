dea2cfe153b7d3854046261c7fbd989360ce834f
Phat Duong <beebapcay@gmail.com>
2022-06-26 18:36:58 +0700

    Add rule delete wallet whenever running app (Need improved)

--- app/blockchain-node/src/pre-start/index.ts
+++ app/blockchain-node/src/pre-start/index.ts
@@ -3,20 +3,26 @@
  * This is useful for environment variables, command-line arguments, and cron-jobs.
  */
 
-import commandLineArgs from 'command-line-args';
-import dotenv from 'dotenv';
-import path from 'path';
+import { WalletRepo } from "@repos/wallet.repo";
+import commandLineArgs from "command-line-args";
+import dotenv from "dotenv";
+import path from "path";
 
+// TODO: Need to be refactored to use more modern way of handling wallet
+(async () => {
+    // Delete previous wallet
+    await WalletRepo.delete();
+})();
 
 (() => {
     // Setup command line options
     const options = commandLineArgs([
         {
-            name: 'env',
-            alias: 'e',
-            defaultValue: 'development',
-            type: String,
-        },
+            name: "env",
+            alias: "e",
+            defaultValue: "development",
+            type: String
+        }
     ]);
     // Set the env file
     const resultConfigEnv = dotenv.config({
