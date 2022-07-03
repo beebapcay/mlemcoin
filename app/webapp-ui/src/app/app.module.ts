def7bb00c1e1e9a45b774be2433368f2b8ad8131
Phat Duong <beebapcay@gmail.com>
2022-07-03 18:37:33 +0700

    Update `angular` version 14 and add `primeNG`

--- app/webapp-ui/src/app/app.module.ts
+++ app/webapp-ui/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { PrimengModule } from './primeng.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/webapp-ui/src/app/primeng.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  exports: [
+    ButtonModule
+  ]
+})
+export class PrimengModule {
+}
