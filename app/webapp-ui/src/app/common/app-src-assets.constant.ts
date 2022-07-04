0e6a18059debd81fe71f30d86fc1ed3a8ceb12b9
Phat Duong <beebapcay@gmail.com>
2022-07-04 12:48:18 +0700

    Add `logo` and `app_name` images assets

--- app/webapp-ui/src/app/common/app-src-assets.constant.ts
+++ app/webapp-ui/src/app/common/app-src-assets.constant.ts
@@ -1,5 +1,15 @@
 export class AppSrcAssetsConstant {
-  // resource for error page
+  // Logo
+  public static readonly LOGO = 'assets/images/logo.png';
+  public static readonly LOGO_CYAN = 'assets/images/logo_cyan.png';
+  public static readonly LOGO_PURPLE = 'assets/images/logo_purple.png';
+
+  // App name
+  public static readonly APP_NAME = 'assets/images/app_name.png';
+  public static readonly APP_NAME_DIMBLUE = 'assets/images/app_dimblue.png';
+  public static readonly APP_NAME_DIMBLACK = 'assets/images/app_dimblack.png';
+
+  // Resource for error page
   public static ERROR_404_1 = 'assets/images/err_404_1.png';
   public static ERROR_404_2 = 'assets/images/err_404_2.png';
 }
